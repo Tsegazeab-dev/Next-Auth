7fd754fb1b1ae648c749eebfe57912f8354222fd
Tsegazeab <tsegazeabwuta@gmail.com>
2023-10-30 13:53:31 -0700

    rewrite type errors

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -44,7 +44,7 @@ export default function LoginForm (){
     return (
         <form onSubmit={handleSubmit} className="flex flex-col space-y-12 w-full px-32">
             <input type="text" value={email} placeholder="Enter your email" onChange={handleEmailChange} className="border-b border-b-gray-200 hover:border-b-gray-500"/>
-            <input type="password" value={password} placeholder="Enter your email" onChange={handlePasswordChange} className="border-b border-b-gray-200 hover:border-b-gray-500"/>
+            <input type="password" value={password} placeholder="Enter your password" onChange={handlePasswordChange} className="border-b border-b-gray-200 hover:border-b-gray-500"/>
             <button type="submit" className="border rounded-lg px-6 py-2 bg-gray-100 hover:bg-gray-200 duration-300 uppercase text-sm">Login</button>
             {error && <p className="text-red-500 font-bold text-center"> {error}</p>}
         </form>
